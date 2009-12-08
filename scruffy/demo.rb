c99bd274b982b1b8ed6c7e4a24f6143ba58ede1f
Chris Lowis <chris.lowis@gmail.com>
2009-12-08 21:46:39 +0000

    Example for scruffy

--- scruffy/demo.rb
+++ scruffy/demo.rb
@@ -8,5 +8,7 @@ graph.renderer = Scruffy::Renderers::Standard.new
 graph.add :line, 'Example', [20, 100, 70, 30, 106]
 
 graph.render :to => "line_test.svg"
+
+# To render png format, you'll need rmagick
 graph.render :width => 300, :height => 200,
 :to => "line_test.png", :as => 'png'
