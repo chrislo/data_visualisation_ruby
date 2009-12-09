11db0222dff67945a2f12b62aea45feaee627433
Chris Lowis <chris.lowis@gmail.com>
2009-12-09 22:47:25 +0000

    Formatting

--- gnuplot/demo.rb
+++ gnuplot/demo.rb
@@ -2,7 +2,8 @@ def gnuplot(commands)
   IO.popen("gnuplot", "w") {|io| io.puts commands}
 end
 
-# Create an SVG plot using Gnuplot's built-in mathematical functions
+# Create an SVG plot using Gnuplot's
+# built-in mathematical functions
 commands =
   %Q(
      set terminal svg
@@ -10,4 +11,6 @@ commands =
      plot [-10:10] sin(x),atan(x),cos(atan(x))
     )
 gnuplot(commands)
+
+
 
